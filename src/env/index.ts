e2823fb320ec88ac7af4d69ee1f088ad5d9000e7
Flávio <f.augustosdn@gmail.com>
2024-10-06 22:45:57 -0300

    iniciando projeto

---
+++ src/env/index.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
+    PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+    console.error('Variable error' + _env.error.format())
+    throw new Error('Invalid environment variable!')
+}
+
+export const env = _env.data
